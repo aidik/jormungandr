b199e5409e77cc5f889865a3a0de88b2ee14ef64
dkijania <dariusz.kijania@iohk.io>
2019-08-05 11:22:45 +0200

    Move shutdown method to jormungandr drop trait to make it more secure (shutdown signal now won't be called in isolation)

--- jormungandr-integration-tests/src/common/jormungandr/process.rs
+++ jormungandr-integration-tests/src/common/jormungandr/process.rs
@@ -1,6 +1,7 @@
 use super::logger::JormungandrLogger;
 use crate::common::configuration::jormungandr_config::JormungandrConfig;
 use crate::common::jcli_wrapper;
+use crate::common::{process_assert, process_utils};
 use std::path::PathBuf;
 use std::process::Child;
 
@@ -54,9 +55,14 @@ impl Drop for JormungandrProcess {
         self.logger.print_error_and_invalid_logs();
         jcli_wrapper::assert_shutdown_node(&self.config.get_node_address());
         self.logger.print_logs_if_contain_error();
-        match self.child.kill() {
-            Err(e) => println!("Could not kill {}: {}", self.description, e),
-            Ok(_) => println!("Successfully killed {}", self.description),
-        }
     }
+}
+
+/// Method sends shutdown signal to jormungandr REST API
+/// WARNING: It asserts that REST API response is ok, it does not verify if its still up
+fn assert_shutdown_node(host: &str) {
+    let output = process_utils::run_process_and_get_output(
+        jcli_wrapper::jcli_commands::get_rest_shutdown_node_command(&host),
+    );
+    process_assert::assert_process_exited_successfully(output);
 }
